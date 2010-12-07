59611fd9b4a60bd6acb03c8c333bb517c3b15fb8
gbois <gbois@dev.java.net>
2010-12-07 23:05:07 +0000

    Updated to latest xunit version + Restored backward compatibility for the usage of a custom stylesheet

--- src/main/java/hudson/plugins/cppunit/CppUnitPublisher.java
+++ src/main/java/hudson/plugins/cppunit/CppUnitPublisher.java
@@ -2,6 +2,7 @@ package hudson.plugins.cppunit;
 
 import com.thalesgroup.dtkit.metrics.hudson.api.type.TestType;
 import com.thalesgroup.hudson.plugins.xunit.XUnitPublisher;
+import com.thalesgroup.hudson.plugins.xunit.types.CustomType;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Recorder;
 
@@ -11,9 +12,13 @@ import java.io.Serializable;
 @SuppressWarnings("unused")
 public class CppUnitPublisher extends Recorder implements Serializable {
 
-    private String testResultsPattern = null;
+    private String testResultsPattern;
 
-    private transient boolean useWorkspaceBaseDir = false;
+    private transient boolean useWorkspaceBaseDir;
+
+    private transient boolean useCustomStylesheet;
+
+    private transient String customStylesheet;
 
     public String getTestResultsPattern() {
         return testResultsPattern;
@@ -23,12 +28,27 @@ public class CppUnitPublisher extends Recorder implements Serializable {
         return useWorkspaceBaseDir;
     }
 
+    public boolean getUseCustomStylesheet() {
+        return useCustomStylesheet;
+    }
+
+    public String getCustomStylesheet() {
+        return customStylesheet;
+    }
+
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.NONE;
     }
 
     private Object readResolve() {
-        return new XUnitPublisher(new TestType[]{new CppUnitPluginType(testResultsPattern, false, true)});
+        TestType[] testTypes = new TestType[1];
+        if (useCustomStylesheet) {
+            testTypes[0] = new CustomType(testResultsPattern, customStylesheet, false, true);
+        } else {
+            testTypes[0] = new CppUnitPluginType(testResultsPattern, false, true);
+        }
+
+        return new XUnitPublisher(testTypes);
     }
 
 }
